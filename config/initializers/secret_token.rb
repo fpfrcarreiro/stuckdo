107ccf53a639cb9e7db0de57fef1b920d83ed315
Fernando Carreiro <fpfr.carreiro@hotmail.com>
2015-01-01 20:54:36 +0000

    Firt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stuckdo::Application.config.secret_token = '80378f5ffd5355d66a9e87176e3b530091ece6e300f913c050d965bf9e03ea28b20357a2b6841844c54aea800b731b6d49391dd10a2f3c5248aa918dbb3be007'
