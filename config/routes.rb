cd8c888924d54be898000dbeb0eecaf009d1703d
Fernando Carreiro <fpfr.carreiro@hotmail.com>
2015-02-23 16:06:32 +0000

    Created a pages controller, and set the route to pages home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Stuckdo::Application.routes.draw do
+  #get 'pages/home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+	
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -48,7 +50,7 @@ Stuckdo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
